0bc45f24df13fdddbb2994e2f7c0d8b444217497
sheilloputratama <sheilloggendut@gmail.com>
2025-05-20 11:02:43 +0700

    membuat pengecekan aksi pop

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -90,3 +90,13 @@ int main()
             cin >> value;
             stact.push(value);
             break;
+        case 2:
+            if (!stact.isEmpty())
+            {
+                stact.pop();
+            }
+            else
+            {
+                cout << "Stact is Empty. cannot pop." << endl;
+            }
+            break;
