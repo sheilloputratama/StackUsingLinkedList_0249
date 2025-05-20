425121ff5b6515851562162b1acdf724c583b6a2
sheilloputratama <sheilloggendut@gmail.com>
2025-05-20 11:01:09 +0700

    menangani pilihan push

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -84,3 +84,9 @@ int main()
         cin >> choice;
 
         switch (choice)
+        {
+        case 1:
+            cout << " Enter the value to push : ";
+            cin >> value;
+            stact.push(value);
+            break;
